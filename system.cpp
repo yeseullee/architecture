c055d6509100898935e7f2306af4c25e3b064b22
Mike <mferdman@cs.stonybrook.edu>
2017-03-11 04:24:48 -0500

    fix up pointers for many syscalls

--- system.cpp
+++ system.cpp
@@ -78,7 +78,7 @@ System::System(Vtop* top, unsigned ramsize, const char* ramelf, const int argc, 
 
 System::~System() {
     free(ram);
-    
+
     if (show_console) {
         sleep(2);
         endwin();
@@ -97,12 +97,12 @@ void System::console() {
 }
 
 void System::tick(int clk) {
-    
+
     if (top->reset && top->bus_reqcyc) {
         cerr << "Sending a request on RESET. Ignoring..." << endl;
         return;
     }
-    
+
     if (!clk) {
         if (top->bus_reqcyc) {
             // hack: blocks ACK if /any/ memory channel can't accept transaction
@@ -171,7 +171,7 @@ void System::tick(int clk) {
             rx_count = 1;
         else
             rx_count = 0;
-            
+
         switch(cmd) {
         case MEMORY:
             xfer_addr = top->bus_req;
@@ -180,8 +180,8 @@ void System::tick(int clk) {
                 cerr << "Access for " << std::hex << xfer_addr << " already outstanding. Ignoring..." << endl;
             } else {
                 assert(
-                    dramsim->addTransaction(isWrite, xfer_addr)
-                );
+                        dramsim->addTransaction(isWrite, xfer_addr)
+                      );
                 //cerr << "add transaction " << std::hex << xfer_addr << " on tag " << top->bus_reqtag << endl;
                 if (!isWrite) addr_to_tag[xfer_addr] = top->bus_reqtag;
             }
@@ -196,8 +196,7 @@ void System::tick(int clk) {
         default:
             assert(0);
         };
-    }
-    else {
+    } else {
         top->bus_reqack = 0;
         rx_count = 0;
     }
@@ -217,7 +216,7 @@ void System::dram_write_complete(unsigned id, uint64_t address, uint64_t clock_c
 }
 
 uint64_t System::load_elf(const char* filename) {
-    
+
     // check libelf version
     if (elf_version(EV_CURRENT) == EV_NONE) {
         cerr << "ELF binary out of date" << endl;
@@ -227,7 +226,7 @@ uint64_t System::load_elf(const char* filename) {
     // open the elf file
     int fileDescriptor = open(filename, O_RDONLY);
     assert(fileDescriptor != -1);
-        
+
     // start reading the file
     Elf* elf = elf_begin(fileDescriptor, ELF_C_READ, NULL);
     if (NULL == elf) {
@@ -256,51 +255,51 @@ uint64_t System::load_elf(const char* filename) {
             assert(shdr.sh_size == read(fileDescriptor, (void*)(ram + 0/* addr */), shdr.sh_size));
             break; // just load the first one
         }
-	} else {
-		for (unsigned phn = 0; phn < elf_header.e_phnum; phn++) {
-			GElf_Phdr phdr;
-			gelf_getphdr(elf, phn, &phdr);
+    } else {
+        for (unsigned phn = 0; phn < elf_header.e_phnum; phn++) {
+            GElf_Phdr phdr;
+            gelf_getphdr(elf, phn, &phdr);
 
-			switch(phdr.p_type) {
-			case PT_LOAD:
-				if ((phdr.p_vaddr + phdr.p_memsz) > ramsize) {
-					cerr << "Not enough 'physical' ram" << endl;
-					exit(-1);
-				}
+            switch(phdr.p_type) {
+            case PT_LOAD:
+                if ((phdr.p_vaddr + phdr.p_memsz) > ramsize) {
+                    cerr << "Not enough 'physical' ram" << endl;
+                    exit(-1);
+                }
 
-				// initialize the memory segment to zero
-				memset(ram + phdr.p_vaddr, 0, phdr.p_memsz);
-				// copy segment content from file to memory
-				assert(-1 != lseek(fileDescriptor, phdr.p_offset, SEEK_SET));
-				assert(phdr.p_filesz == read(fileDescriptor, (void*)(ram + phdr.p_vaddr), phdr.p_filesz));
+                // initialize the memory segment to zero
+                memset(ram + phdr.p_vaddr, 0, phdr.p_memsz);
+                // copy segment content from file to memory
+                assert(-1 != lseek(fileDescriptor, phdr.p_offset, SEEK_SET));
+                assert(phdr.p_filesz == read(fileDescriptor, (void*)(ram + phdr.p_vaddr), phdr.p_filesz));
 
-				if (max_elf_addr < (phdr.p_vaddr + phdr.p_filesz))
-					max_elf_addr = (phdr.p_vaddr + phdr.p_filesz);
+                if (max_elf_addr < (phdr.p_vaddr + phdr.p_filesz))
+                    max_elf_addr = (phdr.p_vaddr + phdr.p_filesz);
 
-				cerr << "Loaded ELF header #" << phn << "."
-					<< " offset: "   << phdr.p_offset
-					<< " filesize: " << phdr.p_filesz
-					<< " memsize: "  << phdr.p_memsz
-					<< " vaddr: "    << std::hex << phdr.p_vaddr << std::dec
-					<< " paddr: "    << std::hex << phdr.p_paddr << std::dec
-					<< " align: "    << phdr.p_align
-					<< endl;
-			    break;
+                cerr << "Loaded ELF header #" << phn << "."
+                    << " offset: "   << phdr.p_offset
+                    << " filesize: " << phdr.p_filesz
+                    << " memsize: "  << phdr.p_memsz
+                    << " vaddr: "    << std::hex << phdr.p_vaddr << std::dec
+                    << " paddr: "    << std::hex << phdr.p_paddr << std::dec
+                    << " align: "    << phdr.p_align
+                    << endl;
+                break;
             case PT_NOTE:
             case PT_TLS:
-			case PT_GNU_STACK:
-			case PT_GNU_RELRO:
-				// do nothing
+            case PT_GNU_STACK:
+            case PT_GNU_RELRO:
+                // do nothing
                 break;
-			default:
-				cerr << "Unexpected ELF header " << phdr.p_type << endl;
-				exit(-1);
-			}
-		}
+            default:
+                cerr << "Unexpected ELF header " << phdr.p_type << endl;
+                exit(-1);
+            }
+        }
 
-		// page-align max_elf_addr
-		max_elf_addr = ((max_elf_addr + 4095) / 4096) * 4096;
-	}
+        // page-align max_elf_addr
+        max_elf_addr = ((max_elf_addr + 4095) / 4096) * 4096;
+    }
 
     // finalize
     close(fileDescriptor);
@@ -311,31 +310,293 @@ extern "C" {
 
 #define ECALL_DEBUG 0
 
-void do_ecall(long long a7, long long a0, long long a1, long long a2, long long a3, long long a4, long long a5, long long a6, long long* a0ret) {
-    switch(a7) {
-    case __NR_munmap:
-        *a0ret = 0; // don't bother unmapping
-        return;
-    case __NR_brk:
-        if (ECALL_DEBUG) cerr << "Allocate " << a0 << " bytes at 0x" << std::hex << ecall_brk << std::dec << endl;
-        *a0ret = ecall_brk;
-        ecall_brk += a0;
-        return;
+    void do_ecall(long long a7, long long a0, long long a1, long long a2, long long a3, long long a4, long long a5, long long a6, long long* a0ret) {
+        switch(a7) {
 
-    case __NR_mmap:
-        assert(a0 == 0 && (a3 & MAP_ANONYMOUS)); // only support ANONYMOUS mmap with NULL argument
-        return do_ecall(__NR_brk,a1,0,0,0,0,0,0,a0ret);
+        case __NR_munmap:
+            *a0ret = 0; // don't bother unmapping
+            return;
+        case __NR_brk:
+            if (ECALL_DEBUG) cerr << "Allocate " << a0 << " bytes at 0x" << std::hex << ecall_brk << std::dec << endl;
+            *a0ret = ecall_brk;
+            ecall_brk += a0;
+            return;
 
-    case __NR_write:
-        a1 += ecall_ram;
-        break;
+        case __NR_mmap:
+            assert(a0 == 0 && (a3 & MAP_ANONYMOUS)); // only support ANONYMOUS mmap with NULL argument
+            return do_ecall(__NR_brk,a1,0,0,0,0,0,0,a0ret);
 
-    default:
-        if (ECALL_DEBUG) cerr << "Default syscall " << a7 << endl;
-        break;
+        case __NR_open:
+        case __NR_poll:
+        case __NR_access:
+        case __NR_pipe:
+        case __NR_uname:
+        case __NR_shmdt:
+        case __NR_truncate:
+        case __NR_getcwd:
+        case __NR_chdir:
+        case __NR_mkdir:
+        case __NR_rmdir:
+        case __NR_creat:
+        case __NR_unlink:
+        case __NR_chmod:
+        case __NR_chown:
+        case __NR_lchown:
+        case __NR_sysinfo:
+        case __NR_times:
+        case __NR_rt_sigpending:
+        case __NR_rt_sigsuspend:
+        case __NR_mknod:
+        case __NR__sysctl:
+        case __NR_adjtimex:
+        case __NR_chroot:
+        case __NR_acct:
+        case __NR_umount2:
+        case __NR_swapon:
+        case __NR_swapoff:
+        case __NR_sethostname:
+        case __NR_setdomainname:
+        case __NR_delete_module:
+        case __NR_time:
+        case __NR_set_tid_address:
+        case __NR_mq_unlink:
+        case __NR_set_robust_list:
+        case __NR_pipe2:
+        case __NR_perf_event_open:
+        case __NR_getrandom:
+        case __NR_memfd_create:
+            a0 += ecall_ram;
+            break;
+
+        case __NR_read:
+        case __NR_write:
+        case __NR_fstat:
+        case __NR_pread64:
+        case __NR_pwrite64:
+        case __NR_readv:
+        case __NR_writev:
+        case __NR_shmat:
+        case __NR_getitimer:
+        case __NR_connect:
+        case __NR_sendmsg:
+        case __NR_recvmsg:
+        case __NR_bind:
+        case __NR_semop:
+        case __NR_msgsnd:
+        case __NR_msgrcv:
+        case __NR_getdents:
+        case __NR_getrlimit:
+        case __NR_getrusage:
+        case __NR_syslog:
+        case __NR_getgroups:
+        case __NR_setgroups:
+        case __NR_ustat:
+        case __NR_fstatfs:
+        case __NR_sched_setparam:
+        case __NR_sched_getparam:
+        case __NR_sched_rr_get_interval:
+        case __NR_modify_ldt:
+        case __NR_setrlimit:
+        case __NR_iopl:
+        case __NR_flistxattr:
+        case __NR_fremovexattr:
+        case __NR_io_setup:
+        case __NR_getdents64:
+        case __NR_timer_gettime:
+        case __NR_clock_settime:
+        case __NR_clock_gettime:
+        case __NR_clock_getres:
+        case __NR_epoll_wait:
+        case __NR_set_mempolicy:
+        case __NR_mq_notify:
+        case __NR_inotify_add_watch:
+        case __NR_openat:
+        case __NR_mkdirat:
+        case __NR_mknodat:
+        case __NR_fchownat:
+        case __NR_unlinkat:
+        case __NR_fchmodat:
+        case __NR_faccessat:
+        case __NR_vmsplice:
+        case __NR_signalfd:
+        case __NR_timerfd_gettime:
+        case __NR_signalfd4:
+        case __NR_preadv:
+        case __NR_pwritev:
+        case __NR_clock_adjtime:
+        case __NR_sendmmsg:
+        case __NR_finit_module:
+        case __NR_sched_setattr:
+        case __NR_sched_getattr:
+        case __NR_bpf:
+            a1 += ecall_ram;
+            break;
+
+        case __NR_stat:
+        case __NR_lstat:
+        case __NR_nanosleep:
+        case __NR_rename:
+        case __NR_link:
+        case __NR_symlink:
+        case __NR_readlink:
+        case __NR_gettimeofday:
+        case __NR_sigaltstack:
+        case __NR_utime:
+        case __NR_statfs:
+        case __NR_pivot_root:
+        case __NR_settimeofday:
+        case __NR_listxattr:
+        case __NR_llistxattr:
+        case __NR_removexattr:
+        case __NR_lremovexattr:
+        case __NR_utimes:
+        case __NR_get_mempolicy:
+            a0 += ecall_ram;
+            a1 += ecall_ram;
+            break;
+
+        case __NR_rt_sigaction:
+        case __NR_rt_sigprocmask:
+        case __NR_setitimer:
+        case __NR_accept:
+        case __NR_getsockname:
+        case __NR_getpeername:
+        case __NR_fsetxattr:
+        case __NR_fgetxattr:
+        case __NR_io_cancel:
+        case __NR_timer_create:
+        case __NR_mq_getsetattr:
+        case __NR_futimesat:
+        case __NR_newfstatat:
+        case __NR_readlinkat:
+        case __NR_utimensat:
+        case __NR_accept4:
+            a1 += ecall_ram;
+            a2 += ecall_ram;
+            break;
+
+        case __NR_setresuid:
+        case __NR_getresuid:
+        case __NR_getresgid:
+        case __NR_rt_sigtimedwait:
+        case __NR_setxattr:
+        case __NR_lsetxattr:
+        case __NR_getxattr:
+        case __NR_lgetxattr:
+        case __NR_add_key:
+        case __NR_request_key:
+        case __NR_getcpu:
+            a0 += ecall_ram;
+            a1 += ecall_ram;
+            a2 += ecall_ram;
+            break;
+
+        case __NR_symlinkat:
+            a0 += ecall_ram;
+            a2 += ecall_ram;
+            break;
+
+        case __NR_futex:
+            a0 += ecall_ram;
+            a3 += ecall_ram;
+            a4 += ecall_ram;
+            break;
+
+        case __NR_select:
+            a1 += ecall_ram;
+            a2 += ecall_ram;
+            a3 += ecall_ram;
+            a4 += ecall_ram;
+            break;
+
+        case __NR_renameat:
+        case __NR_linkat:
+            a1 += ecall_ram;
+            a3 += ecall_ram;
+            break;
+
+        case __NR_recvmmsg:
+        case __NR_sendto:
+            a1 += ecall_ram;
+            a4 += ecall_ram;
+            break;
+
+        case __NR_recvfrom:
+            a1 += ecall_ram;
+            a4 += ecall_ram;
+            a5 += ecall_ram;
+            break;
+
+        case __NR_sendfile:
+            a2 += ecall_ram;
+            break;
+
+        case __NR_socketpair:
+            a3 += ecall_ram;
+            break;
+
+        case __NR_setsockopt:
+            a3 += ecall_ram;
+            break;
+
+        case __NR_getsockopt:
+            a3 += ecall_ram;
+            a4 += ecall_ram;
+            break;
+
+        case __NR_clone:
+        case __NR_get_robust_list:
+        case __NR_execve:
+        case __NR_mincore:
+        case __NR_shmctl:
+        case __NR_wait4:
+        case __NR_msgctl:
+        case __NR_rt_sigqueueinfo:
+        case __NR_sched_setscheduler:
+        case __NR_arch_prctl:
+        case __NR_mount:
+        case __NR_reboot:
+        case __NR_init_module:
+        case __NR_quotactl:
+        case __NR_sched_setaffinity:
+        case __NR_sched_getaffinity:
+        case __NR_io_getevents:
+        case __NR_io_submit:
+        case __NR_semtimedop:
+        case __NR_timer_settime:
+        case __NR_clock_nanosleep:
+        case __NR_epoll_ctl:
+        case __NR_mbind:
+        case __NR_mq_open:
+        case __NR_mq_timedsend:
+        case __NR_mq_timedreceive:
+        case __NR_kexec_load:
+        case __NR_waitid:
+        case __NR_migrate_pages:
+        case __NR_pselect6:
+        case __NR_ppoll:
+        case __NR_splice:
+        case __NR_move_pages:
+        case __NR_epoll_pwait:
+        case __NR_timerfd_settime:
+        case __NR_rt_tgsigqueueinfo:
+        case __NR_prlimit64:
+        case __NR_name_to_handle_at:
+        case __NR_open_by_handle_at:
+        case __NR_process_vm_readv:
+        case __NR_process_vm_writev:
+        case __NR_renameat2:
+        case __NR_seccomp:
+        case __NR_kexec_file_load:
+            cerr << "Unsupported syscall " << a7 << endl;
+            assert(0);
+
+        default:
+            if (ECALL_DEBUG) cerr << "Default syscall " << a7 << endl;
+            break;
+        }
+        if (ECALL_DEBUG) cerr << "Calling syscall " << a7 << endl;
+        *a0ret = syscall(a7, a0, a1, a2, a3, a4, a5, a6);
     }
-    if (ECALL_DEBUG) cerr << "Calling syscall " << a7 << endl;
-    *a0ret = syscall(a7, a0, a1, a2, a3, a4, a5, a6);
-}
 
 }
