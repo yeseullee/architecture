169eabcd5a90fdd53368f04b7e74042761f85828
Shanshan- <shanchen@cs.stonybrook.edu>
2017-04-22 03:14:17 -0400

    implemented new stages in direct_cache.sv

--- wp1/direct_cache.sv
+++ wp1/direct_cache.sv
@@ -9,19 +9,22 @@ module direct_cache
 		INITIAL = 0,
 		ACCEPT = 1,
 		ACKPROC = 2,
-		LOOKUP = 3,
-		DRAM = 4,
-		RECEIVE = 5,
-		UPDATE = 6,
-		RESPOND = 7,
-		RESPACK = 8,
+		READVAL = 3,
+		ACKVAL = 4,
+		LOOKUP = 5,
+		DRAMRD = 6,
+		RECEIVE = 7,
+		UPDATE = 8,
+		RESPOND = 9,
+		RESPACK = 10,
+		DRAMWREQ = 11,
+		DRAMWRT = 12
 
 		//Cache constants
-		CACHE_TAG = 56,		//tag = bits in address (64) - bits in offset (3) - bits in index (5)
+		CACHE_TAG = 55,		//tag = bits in address (64) - bits in offset (4) - bits in index (5)
 		CACHE_INDEX = 5,	//index = log2(32) (# sets in the cache)
-		NUM_CACHE_SETS = 32,
-		OFFSET = 3,			//offset = log2(8) (# bytes in address)
-		CACHE_LENGTH = 569,	//length = valid (1) + tag (56) + data length (512)
+		NUM_CACHE_LINES = 32,
+		OFFSET = 6,			//offset = log2(64) (# addresses in cache line: 8 * 8 sets of 64 bits)
 		DATA_LENGTH = 512
 	)
 	(
@@ -67,29 +70,34 @@ module direct_cache
 	logic [CACHE_TAG-1:0] tag;
 	logic [CACHE_INDEX-1:0] index;
 	logic [OFFSET-1:0] offset;
-	logic [CACHE_LENGTH-1:0] cache[NUM_CACHE_SETS-1:0];
-	logic [CACHE_LENGTH-1:0] _cache[NUM_CACHE_SETS-1:0];
+	logic [NUM_CACHE_LINES-1:0] dirty_bits;
+	logic [NUM_CACHE_LINES-1:0] _dirty_bits;
+	logic [NUM_CACHE_LINES-1:0] valid_bits;
+	logic [NUM_CACHE_LINES-1:0] _valid_bits;
+	logic [CACHE_TAG-1:0] cache_tags[NUM_CACHE_LINES-1:0];
+	logic [CACHE_TAG-1:0] _cache_tags[NUM_CACHE_LINES-1:0];
+	logic [DATA_LENGTH-1:0] cache_data[NUM_CACHE_LINES-1:0];
+	logic [DATA_LENGTH-1:0] _cache_data[NUM_CACHE_LINES-1:0];
 
 	//variables used in RECEIVE and RESPOND to break up content into 8 64-bit blocks
 	logic [8:0] ptr;
 	logic [8:0] next_ptr;
-	logic [63:0] cur_data;		//denotes data currently being transmitted
 
 
 	//NOTE: multiple always comb blocks used to keep verilator happy
 	//	processor resp, ack, and cyc variables cannot be set or used within the same block
    
-	//accept requests from the processor: INITIAL, ACCEPT
+	//accept requests and values from the processor: INITIAL, ACCEPT, READVAL
 	always_comb begin
 		case(state)
 			INITIAL: begin
 					//Initialize the system
 					next_state = ACCEPT;
 				end
-			ACCEPT: begin	//wait for requests from the processor
+			ACCEPT: begin
+					//wait for requests from the processor
 					_req_addr = p_bus_req;
 					_req_tag = p_bus_reqtag;
-					//_content = 0;
 					next_ptr = 0;
 					if(p_bus_reqcyc == 1) begin
 						next_state = ACKPROC;
@@ -98,29 +106,59 @@ module direct_cache
 						next_state = ACCEPT;
 					end
 				end
+			READVAL: begin
+					//read value to be written from processor
+					if(p_bus_reqcyc == 1) begin
+						_content[64*ptr +: 63] = p_bus_req;
+						next_state = ACKVAL;
+					end
+					else begin
+						next_state = READVAL;
+					end
+				end
 		endcase
 	end
 
-	//acknowledge receiving request from processor: ACKPROC
+	//acknowledge receiving request and values from processor: ACKPROC, ACKVAL
 	always_comb begin
 		p_bus_reqack = 0;
 		case(state)
 			ACKPROC: begin
 					p_bus_reqack = 1;
-					next_state = LOOKUP;
+					if(req_tag[11] == 0) begin
+						_content = 0;
+						next_state = READVAL;
+					end
+					else begin
+						next_state = LOOKUP;
+					end
+				end
+			ACKVAL: begin
+					p_bus_reqack = 1;
+					next_ptr = ptr + 1;
+					if(ptr == 7) begin
+						next_state = LOOKUP;
+						next_ptr = 0;
+					end
+					else begin
+						next_state = READVAL;
+					end
 				end
 		endcase
 	end
 
-	//look for requested memory and insert into cache if needed: LOOKUP, DRAM, RECEIVE, UPDATE
+	//look for requested memory and insert into cache if needed: LOOKUP, DRAMRD, RECEIVE, UPDATE
 	always_comb begin
 		//misc related variables
 		m_bus_reqcyc = 0;
 		m_bus_respack = 0;
 		_content = content;
-		for(int i = 0; i < NUM_CACHE_SETS; i++) begin
-			_cache[i] = cache[i];
+		for(int i = 0; i < NUM_CACHE_LINES; i++) begin
+			_cache_data[i] = cache_data[i];
+			_cache_tags[i] = cache_tags[i];
 		end
+		_valid_bits = valid_bits;
+		_dirty_bits = dirty_bits;
 	   
 		//extract tag, index, offset from address
 		tag = req_addr[63:63-CACHE_TAG+1];
@@ -130,24 +168,36 @@ module direct_cache
 		case(state)
 			LOOKUP: begin
 					//compare to existing cache and set next_state and _content respectively
-					next_state = DRAM;
-					/*
-					if(cache[index][CACHE_LENGTH-1] == 1) begin
-						if(cache[index][CACHE_LENGTH-2:DATA_LENGTH] == tag) begin
-							next_state = RESPOND;
-							_content = cache[index][DATA_LENGTH-1:0];
+					if(valid_bits[index] == 1) begin
+						if(cache_tags[index] == tag) begin
+
+							//cache hit on write (invalidate data)
+							if(req_tag[11] == 0) begin
+								_valid_bits[index] = 0;
+								next_state = UPDATE;
+							end
+
+							//cache hit on read
+							else begin
+								next_state = RESPOND;
+								_content = cache_data[index];
+							end
 						end
+
 						else begin
-							next_state = DRAM;
+							//cache miss
+							next_state = DRAMRD;
 							_content = 0;
 						end
 					end
+
 					else begin
-						next_state = DRAM;
+						//cache miss
+						next_state = DRAMRD;
 						_content = 0;
-					end*/
+					end
 				end
-			DRAM: begin
+			DRAMRD: begin
 					//send request to memory
 					m_bus_reqcyc = 1;
 					m_bus_req = req_addr;
@@ -158,7 +208,7 @@ module direct_cache
 						next_state = RECEIVE;
 					end
 					else begin
-						next_state = DRAM;
+						next_state = DRAMRD;
 					end
 				end
 			RECEIVE: begin
@@ -184,9 +234,48 @@ module direct_cache
 			UPDATE: begin
 					//insert the new block into the cache
 					m_bus_respack = 1;
-					//_cache[index] = {1'b1, tag, content};
-					next_state = RESPOND;
+					if(write == 1) begin
+						_dirty_bits[index] == 1;
+						if(valid[index] == 1 && dirty_bits[index] == 1) begin
+							next_state = DRAMWREQ;
+							_content = cache_data[index];
+						end
+						else
+							next_state = ACCEPT;
+						end
+					end
+					else begin
+						_dirty_bits[index] == 0;
+						next_state = RESPOND;
+					end
+
+					_valid_bits[index] = 1;
+					_cache_tags[index] = tag;
+					_cache_data[index] = content;
 				end
+			DRAMWREQ: begin
+					m_bus_reqcyc = 1;
+					m_bus_req = req_addr;
+					if(m_bus_reqack == 1) begin
+							next_ptr = 0;
+							next_state = DRAMWRT;
+					end
+					else begin
+						next_state = DRAMWREQ;
+					end
+				end
+			DRAMWRT: begin
+					m_bus_reqcyc = 1;
+					m_bus_req = content[64*ptr +: 63];
+					if(m_bus_reqack == 1) begin
+						next_ptr = ptr + 1;
+						if(ptr == 7) begin
+							next_state = ACCEPT;
+						end
+						else begin
+							next_state = DRAMWRT;
+						end
+					end
 		endcase
 	end
 
@@ -196,7 +285,7 @@ module direct_cache
 		case(state)
 			RESPOND:begin
 					p_bus_respcyc = 1;
-					p_bus_resp = _content[64*ptr +: 63];
+					p_bus_resp = content[64*ptr +: 63];
 					next_ptr = ptr;
 					next_state = RESPACK;
 				end
@@ -230,8 +319,11 @@ module direct_cache
 			req_tag <= 0;
 			content <= 0;
 			ptr <= 0;
-			for(int i = 0; i < NUM_CACHE_SETS; i++) begin
-				cache[i] <= 0;
+			dirty_bits <= 0;
+			valid_bits <= 0;
+			for(int i = 0; i < NUM_CACHE_LINES; i++) begin
+				cache_data[i] <= 0;
+				cache_tags[i] <= 0;
 			end
 		end
 
@@ -241,8 +333,11 @@ module direct_cache
 		req_tag <= _req_tag;
 		content <= _content;
 		ptr <= next_ptr;
-		for(int i = 0; i < NUM_CACHE_SETS; i++) begin
-			cache[i] <= _cache[i];
+		dirty_bits <= _dirty_bits;
+		valid_bits <= _valid_bits;
+		for(int i = 0; i < NUM_CACHE_LINES; i++) begin
+			cache_data[i] <= _cache_data[i];
+			cache_tags[i] <= _cache_tags[i];
 		end
 	end
 
